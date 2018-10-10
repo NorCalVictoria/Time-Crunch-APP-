1fbfdccd137a20bca25a232943dcf836cc71f4f8
vagrant <vagrant@vagrant.vm>
2018-10-10 07:13:49 +0000

    html pages and partial serverPY

---
+++ model.py
@@ -0,0 +1,79 @@
+"""time crunch database model for app"""
+
+from flask import Flask
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db = SQLAlchemy()
+
+
+#----------------------------------------------------------------------#
+# defs for model
+
+class User(db.Model):
+    """User of time crunch web app."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(20), nullable=True)
+    password = db.Column(db.String(20), nullable=True)
+    fname = db.Column(db.String(20), nullable=True)
+    lname = db.Column(db.String(20), nullable=True)
+   #pets = db.Column(db.String(5), nullable=True)
+   #kids = db.Column(db.String(5), nullable=True)
+    hobbies = db.Column(db.String(20),nullable=False)
+
+    hobbies = db.relationship('Hobby')
+
+    def __repr__(self):
+        """helpful representation"""
+
+        return "<User user_id=%s email=%s>" % (self.user_id, self.email) #MORE FORMATTING NEEDED
+# list of hobbies here.
+
+class Hobby(db.Model):
+    """Hobbies that user chooses from"""
+    __tablename__ = "hobbies"
+
+    hobby_id = db.Column(db.Integer, primary_key=True)
+    kid_friendly = db.Column(db.Boolean, nullable=False)
+    pets_friendly = db.Column(db.Boolean, nullable=False)
+
+    users = db.relationship('User')
+
+class User_Hobby(db.Model):
+    """choices user input when signed up"""
+    __tablename__ = "user_hobbies"
+
+    user_hobby_id= db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+     #another?   = db.Column(db.String, nullable=False)
+
+    users = db.relationship('User')
+
+    def __repr__(self):
+        """ add doc string """
+
+        # return "<>" % ()
+
+#-----------------------------------------------------------------------#
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database."""
+
+    # Configure the PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI']  ('postgresql:///timeCrunch'
+    db.app = app
+    db.init_app(app)
+
+if __name__ == "__main__":
+  
+
+    from server import app
+    connect_to_db(app)
+    print ("Connected to DB.")
---
+++ server.py
@@ -0,0 +1,67 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+
+
+
+
+
+                                    #Routing#
+@app.route('/')
+def index():
+    """homepage"""
+    return render_template('homepage.html')
+
+
+@app.route('/signUp', methods=['GET'])
+def signup_form():
+    """user signup form"""
+
+    return render_template('signUp.html')
+
+
+@app.route('/signUp', methods=['POST'])
+def signup_process():
+    """Process signup"""
+
+                                    #user input#
+    fname = request.form['fname']
+    lname = request.form['lname']
+    email = request.form["email"]
+    password = request.form["password"]
+    
+    
+    new_user = User(fname=fname, lname=lname, password=password)
+
+                                   #add_new_user#
+    db.session.add(new_user)
+    db.session.commit()
+
+    session['user_id'] = new_user.user_id
+    session['fname'] = new_user.fname
+
+    return redirect('/settings')   
+
+
+                                    #Profile Upload# 
+@app.route('/add_profile_img', methods=['GET', 'POST'])
+def upload():
+
+    user_id = session.get('user_id')
+    path = str(user_id) + ".jpg"
+    if request.method == 'POST' and 'photo' in request.files:
+        request.files['photo'].filename = path
+        filename = photos.save(request.files['photo'])
+        user = User.query.get(user_id)
+        user.photo = '/' + app.config['UPLOADED_PHOTOS_DEST'] + '/' + path
+        db.session.commit()
+        
+    
+
+if __name__ == '__main__':
+    # error messages and reload
+    # our web app if we change the code.
+    app.run(debug=True)
